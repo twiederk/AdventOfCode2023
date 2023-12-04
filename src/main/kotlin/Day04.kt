40a4f754f45b22b2650bce3c9a5c24f5afd0361b
Torsten Wiederkehr <torsten.wiederkehr@ing.com>
2023-12-04 07:55:02 +0100

    Implemented calculation of pointd

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -1,3 +1,4 @@
+import java.lang.IllegalArgumentException
 import java.nio.file.Path
 
 class Day04 {
@@ -21,4 +22,25 @@ class Day04 {
 data class Card(
     val winningNumbers: List<Int>,
     val numbers: List<Int>
-)
+) {
+    fun wins(): List<Int> {
+        return numbers.filter { winningNumbers.contains(it) }
+    }
+
+    fun points(): Int =
+        when (wins().size) {
+            0 -> 0
+            1 -> 1
+            2 -> 2
+            3 -> 4
+            4 -> 8
+            5 -> 16
+            6 -> 32
+            7 -> 64
+            8 -> 128
+            9 -> 256
+            10 -> 512
+            else -> throw IllegalArgumentException("Number of wins to large")
+        }
+
+}
--- src/test/kotlin/Day04Test.kt
+++ src/test/kotlin/Day04Test.kt
@@ -17,7 +17,6 @@ class Day04Test {
 
     @Test
     fun should_create_card() {
-        // arrange
 
         // act
         val card = Day04().createCard("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53")
@@ -25,6 +24,54 @@ class Day04Test {
         // assert
         assertThat(card.winningNumbers).containsExactly(41, 48, 83, 86, 17)
         assertThat(card.numbers).containsExactly(83, 86, 6, 31, 17, 9, 48, 53)
+    }
+
+    @Test
+    fun should_count_number_of_wins() {
+        // arrange
+        val card = Card(listOf(41, 48, 83, 86, 17), listOf(83, 86, 6, 31, 17, 9, 48, 53))
+
+        // act
+        val wins = card.wins()
+
+        // assert
+        assertThat(wins).contains(48, 83, 17, 86)
+    }
+
+    @Test
+    fun should_return_points_of_card() {
+        // arrange
+        val card = Card(listOf(41, 48, 83, 86, 17), listOf(83, 86, 6, 31, 17, 9, 48, 53))
+
+        // act
+        val points = card.points()
+
+        // assert
+        assertThat(points).isEqualTo(8)
+    }
+
+    @Test
+    fun should_return_points_of_wins_0() {
+        // arrange
+        val card = Card(listOf(12), listOf(1))
+
+        // act
+        val points = card.points()
+
+        // assert
+        assertThat(points).isEqualTo(0)
+    }
+
+    @Test
+    fun should_return_points_of_wins_1() {
+        // arrange
+        val card = Card(listOf(12), listOf(12))
+
+        // act
+        val points = card.points()
+
+        // assert
+        assertThat(points).isEqualTo(1)
     }
 
 }
