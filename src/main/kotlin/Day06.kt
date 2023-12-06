d8e1237486fbf2f54bf5c4dd35937fab6c793a8a
Torsten Wiederkehr <torsten.wiederkehr@ing.com>
2023-12-06 09:56:37 +0100

    Started with parsing data

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -1,2 +1,14 @@
+import java.nio.file.Path
+
 class Day06 {
+
+    fun loadData(path: Path): List<Race> {
+        return listOf()
+    }
+
 }
+
+data class Race(
+    val time: Int,
+    val distance: Int
+)
--- src/test/kotlin/Day06Test.kt
+++ src/test/kotlin/Day06Test.kt
@@ -1,2 +1,18 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.nio.file.Paths
+
 class Day06Test {
+
+    @Test
+    fun should_load_data() {
+        // arrange
+
+        // act
+        val races = Day06().loadData(Paths.get("src", "test", "resources", "Day06_TestData.txt"))
+
+        // assert
+        assertThat(races).hasSize(3)
+    }
+
 }
