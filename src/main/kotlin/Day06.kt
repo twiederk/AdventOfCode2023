b8a2f831b4a62e43bc3af6093d54a0ff4f006270
Torsten Wiederkehr <torsten.wiederkehr@ing.com>
2023-12-06 14:23:51 +0100

    Implemented loadRaces

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -2,8 +2,17 @@ import java.nio.file.Path
 
 class Day06 {
 
-    fun loadData(path: Path): List<Race> {
-        return listOf()
+    fun loadRaces(path: Path): List<Race> {
+        val rawData = Resources.resourceAsListOfString(path.toFile().name)
+        val times = rawData[0].substringAfter("Time:    ").replace("\\s+".toRegex(), "|").split("|").drop(1)
+        println("times: $times")
+        val distances = rawData[1].substringAfter("Distance:").replace("\\s+".toRegex(), "|").split("|").drop(1)
+        println("distances: $distances")
+        val races = mutableListOf<Race>()
+        for (index in times.indices) {
+            races.add(Race(times[index].toInt(), distances[index].toInt()))
+        }
+        return races
     }
 
 }
--- src/test/kotlin/Day06Test.kt
+++ src/test/kotlin/Day06Test.kt
@@ -5,14 +5,18 @@ import java.nio.file.Paths
 class Day06Test {
 
     @Test
-    fun should_load_data() {
+    fun should_load_races() {
         // arrange
 
         // act
-        val races = Day06().loadData(Paths.get("src", "test", "resources", "Day06_TestData.txt"))
+        val races = Day06().loadRaces(Paths.get("src", "test", "resources", "Day06_TestData.txt"))
 
         // assert
-        assertThat(races).hasSize(3)
+        assertThat(races).containsExactly(
+            Race(7, 9),
+            Race(15, 40),
+            Race(30, 200),
+        )
     }
 
 }
