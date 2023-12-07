3c943f47c15cbfbb95f01dc397bed2843265ada9
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2023-12-07 06:58:00 +0100

    Implemented loading of hands

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -1,2 +1,18 @@
+import java.nio.file.Path
+
 class Day07 {
+
+    fun loadHands(path: Path): List<Hand> {
+        val data = Resources.resourceAsListOfString(path.toFile().name)
+        return data.map {
+            val result = it.split(" ")
+            Hand(result[0], result[1].toInt())
+        }
+    }
+
 }
+
+data class Hand(
+    val cards: String,
+    val bid: Int
+)
--- src/test/kotlin/Day07Test.kt
+++ src/test/kotlin/Day07Test.kt
@@ -1,2 +1,23 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.nio.file.Paths
+
 class Day07Test {
+
+    @Test
+    fun should_load_hands() {
+
+        // act
+        val hands = Day07().loadHands(Paths.get("src", "test", "resources", "Day07_TestData.txt"))
+
+        // assert
+        assertThat(hands).contains(
+            Hand("32T3K", 765),
+            Hand("T55J5", 684),
+            Hand("KK677", 28),
+            Hand("KTJJT", 220),
+            Hand("QQQJA", 483),
+        )
+    }
+
 }
