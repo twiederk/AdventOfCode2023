ad93ff0ab8424c5f5a80e3f8e6f3c49642171c37
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2023-12-08 08:49:14 +0100

    Solved Day08 part 1

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -1,4 +1,5 @@
 import java.nio.file.Path
+import java.nio.file.Paths
 
 class Day08 {
 
@@ -17,10 +18,41 @@ class Day08 {
                     )
         }
     }
+
+    fun countSteps(instructions: String, nodes: Map<String, Node>): Int {
+        var node: Node = nodes.getOrElse("AAA") { throw IllegalArgumentException("Node not found: AAA") }
+        var steps = 0
+        while (true) {
+            val instruction = instructions.nth(steps)
+            steps++
+            node = nodes.getOrElse(node.next(instruction)) { throw IllegalArgumentException("Node not found: AAA") }
+            if (node.name == "ZZZ") {
+                return steps
+            }
+        }
+        return -1
+    }
 }
 
 data class Node(
     val name: String,
     val left: String,
     val right: String
-)
+) {
+    fun next(instruction: Char): String {
+        return when (instruction) {
+            'L' -> left
+            else -> right
+        }
+    }
+}
+
+fun main() {
+    val day08 = Day08()
+    val path = Paths.get("src", "main", "resources", "Day08_InputData.txt")
+    val instructions = day08.loadInstructions(path)
+    val nodes = day08.loadNodes(path)
+
+    val part1 = day08.countSteps(instructions, nodes)
+    println("Day08 part1: $part1")
+}
--- src/test/kotlin/Day08Test.kt
+++ src/test/kotlin/Day08Test.kt
@@ -33,4 +33,75 @@ class Day08Test {
         )
     }
 
+    @Test
+    fun should_return_key_of_left_node() {
+
+        // act
+        val nodeKey = Node("AAA", "BBB", "CCC").next('L')
+
+        // assert
+        assertThat(nodeKey).isEqualTo("BBB")
+
+    }
+
+    @Test
+    fun should_return_key_of_right_node() {
+
+        // act
+        val nodeKey = Node("AAA", "BBB", "CCC").next('R')
+
+        // assert
+        assertThat(nodeKey).isEqualTo("CCC")
+    }
+
+    @Test
+    fun should_count_steps_of_example_1() {
+        // arrange
+        val nodes = mapOf(
+            "AAA" to Node("AAA", "BBB", "CCC"),
+            "BBB" to Node("BBB", "DDD", "EEE"),
+            "CCC" to Node("CCC", "ZZZ", "GGG"),
+            "DDD" to Node("DDD", "DDD", "DDD"),
+            "EEE" to Node("EEE", "EEE", "EEE"),
+            "GGG" to Node("GGG", "GGG", "GGG"),
+            "ZZZ" to Node("ZZZ", "ZZZ", "ZZZ"),
+        )
+        val instructions = "RL"
+
+        // act
+        val steps = Day08().countSteps(instructions, nodes)
+
+        // assert
+        assertThat(steps).isEqualTo(2)
+    }
+
+    @Test
+    fun should_count_steps_of_example_2() {
+        // arrange
+        val nodes = mapOf(
+            "AAA" to Node("AAA", "BBB", "BBB"),
+            "BBB" to Node("BBB", "AAA", "ZZZ"),
+            "ZZZ" to Node("ZZZ", "ZZZ", "ZZZ"),
+        )
+        val instructions = "LLR"
+
+        // act
+        val steps = Day08().countSteps(instructions, nodes)
+
+        // assert
+        assertThat(steps).isEqualTo(6)
+    }
+
+    @Test
+    fun should_cycle_index_through_list() {
+        // arrange
+        val instructions = "LLR"
+
+        // act
+        val instruction = instructions.nth(5)
+
+        // assert
+        assertThat(instruction).isEqualTo('R')
+    }
+
 }
