e4782ed1af9429c6a61644329a59cb964a96ae02
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2023-12-08 06:10:20 +0100

    Load instruction set

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -1,2 +1,8 @@
+import java.nio.file.Path
+
 class Day08 {
+
+    fun loadInstructions(path: Path): String {
+        return Resources.resourceAsListOfString(path.toFile().name)[0]
+    }
 }
--- src/test/kotlin/Day08Test.kt
+++ src/test/kotlin/Day08Test.kt
@@ -1,2 +1,18 @@
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import java.nio.file.Paths
+
 class Day08Test {
+
+    @Test
+    fun should_load_instructions() {
+        // arrange
+
+        // act
+        val instructions = Day08().loadInstructions(Paths.get("src", "test", "resources", "Day08_TestData.txt"))
+
+        // assert
+        Assertions.assertThat(instructions).isEqualTo("RL")
+
+    }
 }
