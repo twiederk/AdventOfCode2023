0141cd12679a5e4266385cb0888272006a0b303c
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2023-12-09 18:19:01 +0100

    Solved Day09 part1

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -1,22 +1,28 @@
 import java.nio.file.Path
+import java.nio.file.Paths
 
 class Day09 {
+
     fun loadHistories(path: Path): List<List<Int>> {
         val linesOfHistories = Resources.resourceAsListOfString(path.toFile().name)
-        return linesOfHistories.map { historyLine ->
-            historyLine.split(" ").map { historyValue -> historyValue.toInt() }
-        }
+        return linesOfHistories.map { line -> line.split(" ").map { value -> value.toInt() } }
     }
 
-    fun predictNextValue(history: List<Long>): Long {
-        if (history.sum() == 0L) return 0L
+    fun predictNextValue(history: List<Int>): Int {
+        if (history.all { it == 0 }) return 0
         val nextDiff = history.windowed(2).map { it[1] - it[0] }
         return history.last() + predictNextValue(nextDiff)
     }
 
-    fun sumOfNextValues(histories: List<List<Long>>): Long {
+    fun sumOfNextValues(histories: List<List<Int>>): Int {
         return histories.sumOf { predictNextValue(it) }
     }
 
 }
 
+fun main() {
+    val day09 = Day09()
+    val histories = day09.loadHistories(Paths.get("src", "main", "resources", "Day09_InputData.txt"))
+    val part1 = day09.sumOfNextValues(histories)
+    println("Day09 part1: $part1")
+}
