dedfc77386f79cac3787d288388d0dbaf9188881
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2023-12-09 07:26:39 +0100

    Implemented loadHistories()

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -1,2 +1,16 @@
+import java.nio.file.Path
+
 class Day09 {
+    fun loadHistories(path: Path): List<List<Int>> {
+        val histories = mutableListOf<List<Int>>()
+        val listOfStrings = Resources.resourceAsListOfString(path.toFile().name)
+        for (string in listOfStrings) {
+            val splitted = string.split(" ")
+            val history = splitted.map { it.toInt() }
+            histories.add(history)
+        }
+        return histories
+    }
+
 }
+
--- src/test/kotlin/Day09Test.kt
+++ src/test/kotlin/Day09Test.kt
@@ -1,5 +1,20 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.nio.file.Paths
+
 class Day09Test {
 
 //    1 -2 -13 -26 -23 28 189 597 1553 3670 8095 16820 33097 61972 110953 190827 316641 508862 794731 1209826 1799849
+
+    @Test
+    fun should_load_histories() {
+        // arrange
+
+        // act
+        val histories = Day09().loadHistories(Paths.get("src", "test", "resources", "Day09_TestData.txt"))
+
+        // assert
+        assertThat(histories).hasSize(3)
+    }
 
 }
