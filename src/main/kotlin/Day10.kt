e3ee73b9b8c2e1c60eb1815025ae6e8eb6dd4998
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2023-12-10 06:15:36 +0100

    Implemented loadMaze()

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -1,2 +1,10 @@
+import java.nio.file.Path
+
+typealias Maze = List<String>
+
 class Day10 {
+
+    fun loadMaze(path: Path): Maze {
+        return Resources.resourceAsListOfString(path.toFile().name)
+    }
 }
--- src/test/kotlin/Day10Test.kt
+++ src/test/kotlin/Day10Test.kt
@@ -1,2 +1,17 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.nio.file.Paths
+
 class Day10Test {
+
+    @Test
+    fun should_load_maze() {
+
+        // act
+        val maze = Day10().loadMaze(Paths.get("src", "test", "resources", "Day10_TestData.txt"))
+
+        // assert
+        assertThat(maze).hasSize(5)
+    }
+
 }
