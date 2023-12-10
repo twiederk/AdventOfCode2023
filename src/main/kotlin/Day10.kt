629863160f1d031217b71f4ae92a9055c30bdcd0
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2023-12-10 06:28:05 +0100

    Implemented find of starting position

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -7,4 +7,15 @@ class Day10 {
     fun loadMaze(path: Path): Maze {
         return Resources.resourceAsListOfString(path.toFile().name)
     }
+
+    fun startingPosition(maze: Maze): Point {
+        for ((row, line) in maze.withIndex()) {
+            for ((column, pipe) in line.withIndex()) {
+                if (pipe == 'S') {
+                    return Point(column, row)
+                }
+            }
+        }
+        throw IllegalStateException("Can't find starting point")
+    }
 }
--- src/test/kotlin/Day10Test.kt
+++ src/test/kotlin/Day10Test.kt
@@ -4,6 +4,22 @@ import java.nio.file.Paths
 
 class Day10Test {
 
+    val simpleMaze: Maze = listOf(
+        "-L|F7",
+        "7S-7|",
+        "L|7||",
+        "-L-J|",
+        "L|-JF",
+    )
+
+    val complexMaze: Maze = listOf(
+        "7-F7-",
+        ".FJ|7",
+        "SJLL7",
+        "|F--J",
+        "LJ.LJ",
+    )
+
     @Test
     fun should_load_maze() {
 
@@ -14,4 +30,37 @@ class Day10Test {
         assertThat(maze).hasSize(5)
     }
 
+    //    01234
+//  0 -L|F7
+//  1 7S-7|
+//  2 L|7||
+//  3 -L-J|
+//  4 L|-JF
+    @Test
+    fun should_find_starting_position_of_simple_maze() {
+
+        // act
+        val startingPosition = Day10().startingPosition(simpleMaze)
+
+        // assert
+        assertThat(startingPosition).isEqualTo(Point(1, 1))
+
+    }
+
+    //   01234
+// 0 7-F7-
+// 1 .FJ|7
+// 2 SJLL7
+// 3 |F--J
+// 4 LJ.LJ
+    @Test
+    fun should_find_starting_position_of_complex_maze() {
+
+        // act
+        val startingPosition = Day10().startingPosition(complexMaze)
+
+        // assert
+        assertThat(startingPosition).isEqualTo(Point(0, 2))
+
+    }
 }
