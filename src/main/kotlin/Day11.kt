926bbdeaf10253871c7c88068cff1521d70f475e
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2023-12-11 18:21:25 +0100

    Find index of rows to expand

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -4,4 +4,12 @@ class Day11 {
     fun loadGalaxyImage(path: Path): List<String> {
         return Resources.resourceAsListOfString(path.toFile().name)
     }
+
+    fun rowsToExpand(galaxyImage: List<String>): List<Int> {
+        val rowsToExpand = mutableListOf<Int>()
+        galaxyImage.forEachIndexed { index, line ->
+            if (line.all { it == '.' }) rowsToExpand.add(index)
+        }
+        return rowsToExpand
+    }
 }
--- src/test/kotlin/Day11Test.kt
+++ src/test/kotlin/Day11Test.kt
@@ -14,4 +14,27 @@ class Day11Test {
         assertThat(galaxyImage).hasSize(10)
     }
 
+    @Test
+    fun should_return_rows_to_expand() {
+        // arrange
+        val galaxyImage = listOf(
+            "...#......",
+            ".......#..",
+            "#.........",
+            "..........",
+            "......#...",
+            ".#........",
+            ".........#",
+            "..........",
+            ".......#..",
+            "#...#.....",
+        )
+
+        // act
+        val rowsToExpand = Day11().rowsToExpand(galaxyImage)
+
+        // assert
+        assertThat(rowsToExpand).containsExactly(3, 7)
+
+    }
 }
