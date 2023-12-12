534aed9d480218230ce2ce99f19381249bdfdb8b
Torsten Wiederkehr <torsten.wiederkehr@ing.com>
2023-12-12 15:27:14 +0100

    Implemented load of condition records

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -1,2 +1,20 @@
+import java.nio.file.Path
+
 class Day12 {
+
+    fun loadConditionRecords(path: Path): List<ConditionRecord> {
+        val rawData = Resources.resourceAsListOfString(path.toFile().name)
+        return rawData.map {
+            ConditionRecord(
+                springs = it.substringBefore(" "),
+                damagedGroups = it.substringAfter(" ").split(',').map { value -> value.toInt() }
+            )
+        }
+    }
+
 }
+
+data class ConditionRecord(
+    val springs: String,
+    val damagedGroups: List<Int>
+)
--- src/test/kotlin/Day12Test.kt
+++ src/test/kotlin/Day12Test.kt
@@ -1,2 +1,26 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.nio.file.Paths
+
 class Day12Test {
+
+    @Test
+    fun should_load_condition_records() {
+        // arrange
+
+        // act
+        val conditionRecords = Day12().loadConditionRecords(Paths.get("src", "test", "resources", "Day12_TestData.txt"))
+
+        // assert
+        assertThat(conditionRecords).hasSize(6)
+        assertThat(conditionRecords).containsExactly(
+            ConditionRecord("???.###", listOf(1, 1, 3)),
+            ConditionRecord(".??..??...?##.", listOf(1, 1, 3)),
+            ConditionRecord("?#?#?#?#?#?#?#?", listOf(1, 3, 1, 6)),
+            ConditionRecord("????.#...#...", listOf(4, 1, 1)),
+            ConditionRecord("????.######..#####.", listOf(1, 6, 5)),
+            ConditionRecord("?###????????", listOf(3, 2, 1)),
+        )
+    }
+
 }
