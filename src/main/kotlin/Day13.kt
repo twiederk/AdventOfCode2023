d12ec37c68887192ae1edb2084543b25f7029ea5
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2023-12-13 06:15:32 +0100

    Implemented load of pattern

--- src/main/kotlin/Day13.kt
+++ src/main/kotlin/Day13.kt
@@ -1,2 +1,22 @@
+import java.nio.file.Path
+
+typealias Pattern = List<String>
+
 class Day13 {
+    fun loadPatterns(path: Path): List<Pattern> {
+        val rawData = Resources.resourceAsListOfString(path.toFile().name)
+
+        val patterns = mutableListOf<Pattern>()
+        var pattern = mutableListOf<String>()
+        for (line in rawData) {
+            if (line.isNotEmpty()) {
+                pattern.add(line)
+            } else {
+                patterns.add(pattern)
+                pattern = mutableListOf()
+            }
+        }
+        patterns.add(pattern)
+        return patterns
+    }
 }
--- src/test/kotlin/Day13Test.kt
+++ src/test/kotlin/Day13Test.kt
@@ -1,2 +1,38 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.nio.file.Paths
+
 class Day13Test {
+
+    private val pattern1 = listOf(
+        "#.##..##.",
+        "..#.##.#.",
+        "##......#",
+        "##......#",
+        "..#.##.#.",
+        "..##..##.",
+        "#.#.##.#.",
+    )
+
+    private val pattern2 = listOf(
+        "#...##..#",
+        "#....#..#",
+        "..##..###",
+        "#####.##.",
+        "#####.##.",
+        "..##..###",
+        "#....#..#",
+    )
+
+    @Test
+    fun should_load_patterns() {
+        // arrange
+
+        // act
+        val patterns = Day13().loadPatterns(Paths.get("src", "test", "resources", "Day13_TestData.txt"))
+
+        // assert
+        assertThat(patterns).hasSize(2)
+    }
+
 }
