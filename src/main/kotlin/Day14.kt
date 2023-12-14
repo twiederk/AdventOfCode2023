a01cc379a9b5eeb7345d7a5220be2391ed0c864e
Torsten Wiederkehr <torsten.wiederkehr@ing.com>
2023-12-14 14:37:24 +0100

    Implemented load platform

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -1,2 +1,9 @@
+import java.nio.file.Path
+
 class Day14 {
+
+    fun loadPlatform(path: Path): List<String> {
+        return Resources.resourceAsListOfString(path.toFile().name)
+    }
+
 }
--- src/test/kotlin/Day14Test.kt
+++ src/test/kotlin/Day14Test.kt
@@ -1,2 +1,16 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.nio.file.Paths
+
 class Day14Test {
+
+    @Test
+    fun should_load_platform() {
+
+        // act
+        val platform = Day14().loadPlatform(Paths.get("src", "test", "resources", "Day14_TestData.txt"))
+
+        // assert
+        assertThat(platform).hasSize(10)
+    }
 }
