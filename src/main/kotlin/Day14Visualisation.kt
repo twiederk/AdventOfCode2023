debcf191d14a36e2297580f98efbdab2ed3ebe27
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2023-12-15 16:30:36 +0100

    Implemented simple visualisation

--- src/main/kotlin/Day14Visualisation.kt
+++ src/main/kotlin/Day14Visualisation.kt
@@ -1,40 +1,39 @@
 import utils.KPixelGameEngine
 import java.awt.Color
+import java.nio.file.Paths
 
 class Day14Visualisation : KPixelGameEngine("AoC in Kotlin 2023 Day 14 - Parabolic Reflector Dish") {
 
-    private val platform = Platform(
-        listOf(
-            "O....#....".toCharArray(),
-            "O.OO#....#".toCharArray(),
-            ".....##...".toCharArray(),
-            "OO.#O....O".toCharArray(),
-            ".O.....O#.".toCharArray(),
-            "O.#..O.#.#".toCharArray(),
-            "..O..#O..O".toCharArray(),
-            ".......O..".toCharArray(),
-            "#....###..".toCharArray(),
-            "#OO..#....".toCharArray(),
-        )
-    )
+    private var platform = Platform(listOf())
 
     override fun onCreate() {
-        construct(150, 100, 5)
+        platform = Day14().loadPlatform(Paths.get("src", "main", "resources", "Day14_InputData.txt"))
+
+        construct(platform.data[0].size, platform.data.size, 5)
         limitFps = 10
+
     }
 
     override fun onUpdate(elapsedTime: Long, frame: Long) {
+        drawPlatform(platform)
+        if (frame % 5 == 0L) {
+            platform = platform.tiltSingleStep()
+        }
+
+    }
+
+    private fun drawPlatform(platform: Platform) {
         for ((y, row) in platform.data.withIndex()) {
             for ((x, char) in row.withIndex()) {
                 when (char) {
-                    'O' -> draw(x, y, Color.BLUE)
+                    'O' -> draw(x, y, Color.YELLOW)
                     '#' -> draw(x, y, Color.BLACK)
                     else -> draw(x, y, Color.LIGHT_GRAY)
                 }
-
             }
         }
     }
+
 }
 
 fun main() {
