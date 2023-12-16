46faa84cb425911e36c7950ced309e2e8f994ed3
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2023-12-16 04:28:06 +0100

    Implemented load of initialization sequence

--- src/main/kotlin/Day15.kt
+++ src/main/kotlin/Day15.kt
@@ -1,2 +1,8 @@
+import java.nio.file.Path
+
 class Day15 {
+
+    fun loadInitializationSequence(path: Path): List<String> {
+        return Resources.resourceAsText(path.toFile().name).split(',')
+    }
 }
--- src/test/kotlin/Day15Test.kt
+++ src/test/kotlin/Day15Test.kt
@@ -1,2 +1,31 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.nio.file.Paths
+
 class Day15Test {
+
+    @Test
+    fun should_load_initialization_sequence() {
+        // arrange
+
+        // act
+        val initializationSequence =
+            Day15().loadInitializationSequence(Paths.get("src", "test", "resources", "Day15_TestData.txt"))
+
+        // assert
+        assertThat(initializationSequence).containsExactly(
+            "rn=1",
+            "cm-",
+            "qp=3",
+            "cm=2",
+            "qp-",
+            "pc=4",
+            "ot=9",
+            "ab=5",
+            "pc-",
+            "pc=6",
+            "ot=7",
+        )
+    }
+
 }
