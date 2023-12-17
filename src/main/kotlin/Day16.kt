18541f17c778164b5416108075230930a903afda
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2023-12-17 05:42:47 +0100

    Implemented load of data

--- src/main/kotlin/Day16.kt
+++ src/main/kotlin/Day16.kt
@@ -1,2 +1,9 @@
+import java.nio.file.Path
+
 class Day16 {
+
+    fun loadContraption(path: Path): List<String> {
+        return Resources.resourceAsListOfString(path.toFile().name)
+    }
+
 }
--- src/test/kotlin/Day16Test.kt
+++ src/test/kotlin/Day16Test.kt
@@ -1,2 +1,17 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.nio.file.Paths
+
 class Day16Test {
+
+    @Test
+    fun should_load_contraption() {
+
+        // act
+        val contraption = Day16().loadContraption(Paths.get("src", "test", "resources", "Day16_TestData.txt"))
+
+        // assert
+        assertThat(contraption).hasSize(10)
+    }
+
 }
