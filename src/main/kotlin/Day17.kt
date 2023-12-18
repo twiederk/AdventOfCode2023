19a566b022fefa602a4eb88f4cc7788cf9756429
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2023-12-18 04:45:47 +0100

    Implemented load heat map

--- src/main/kotlin/Day17.kt
+++ src/main/kotlin/Day17.kt
@@ -1,2 +1,9 @@
+import java.nio.file.Path
+
 class Day17 {
+
+    fun loadHeatMap(path: Path): List<String> {
+        return Resources.resourceAsListOfString(path.toFile().name)
+    }
+
 }
--- src/test/kotlin/Day17Test.kt
+++ src/test/kotlin/Day17Test.kt
@@ -1,2 +1,17 @@
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import java.nio.file.Paths
+
 class Day17Test {
+
+    @Test
+    fun should_load_heat_map() {
+
+        // act
+        val heatMap = Day17().loadHeatMap(Paths.get("src", "test", "resources", "Day17_TestData.txt"))
+
+        // assert
+        Assertions.assertThat(heatMap).hasSize(13)
+
+    }
 }
