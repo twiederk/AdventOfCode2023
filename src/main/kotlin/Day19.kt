46d491b01a352f50462fe61d2ff7962e36950cd2
Torsten Wiederkehr <torsten.wiederkehr@ing.com>
2024-03-14 09:51:58 +0100

    Implemented Workflow

--- src/main/kotlin/Day19.kt
+++ src/main/kotlin/Day19.kt
@@ -12,4 +12,85 @@ class Day19 {
         return lines.subList(lines.indexOf("") + 1, lines.size)
     }
 
+
 }
+
+// qqz{s>2770:qs,m<1801:hdj,R}
+data class Workflow(val line: String) {
+
+    val key: String
+    val rules = mutableListOf<Rule>()
+
+    init {
+        key = line.substringBefore("{")
+        line.substringAfter("{").substringBefore("}").split(',')
+            .forEach { rules.add(Rule(it)) }
+    }
+
+    fun next(part: Part): String {
+        rules.forEach {
+            if (it.field == null) {
+                return it.next
+            }
+            when (it.operator) {
+                '>' -> if (part.getValueOf(it.field!!) > it.value!!) return it.next
+                '<' -> if (part.getValueOf(it.field!!) < it.value!!) return it.next
+            }
+        }
+        throw IllegalStateException("No rule found for part $part")
+    }
+}
+
+// {x=787,m=2655,a=1222,s=2876}
+data class Part(val line: String) {
+
+    val x: Int
+    val m: Int
+    val a: Int
+    val s: Int
+
+    init {
+        val regex = Regex("\\{x=(\\d+),m=(\\d+),a=(\\d+),s=(\\d+)}")
+        val matchResult = regex.find(line)
+        val (x, m, a, s) = matchResult!!.destructured
+        this.x = x.toInt()
+        this.m = m.toInt()
+        this.a = a.toInt()
+        this.s = s.toInt()
+    }
+
+    fun getValueOf(field: String): Int {
+        when (field) {
+            "x" -> return x
+            "m" -> return m
+            "a" -> return a
+            "s" -> return s
+        }
+        throw IllegalArgumentException("Unknown field $field")
+    }
+
+}
+
+// s>2770:qs
+// m<1801:hdj
+// R
+data class Rule(val line: String) {
+
+    var field: String? = null
+    var operator: Char? = null
+    var value: Int? = null
+    val next: String
+
+    init {
+        if (line.contains(':')) {
+            field = line[0].toString()
+            operator = line[1]
+            value = line.substring(2, line.indexOf(':')).toInt()
+            next = line.substringAfter(':')
+        } else {
+            next = line
+        }
+    }
+
+}
+
--- src/test/kotlin/Day19Test.kt
+++ src/test/kotlin/Day19Test.kt
@@ -21,6 +21,79 @@ class Day19Test {
 
         // assert
         assertThat(parts).hasSize(5)
-
     }
+
+    @Test
+    fun should_return_key_of_first_rule_in_workflow() {
+
+        // act
+        val key = Workflow("in{s<1351:px,qqz}").next(Part("{x=787,m=2655,a=1222,s=100}"))
+
+        // assert
+        assertThat(key).isEqualTo("px")
+    }
+
+    @Test
+    fun should_return_key_of_last_rule_in_workflow() {
+
+        // act
+        val key = Workflow("in{s<1351:px,qqz}").next(Part("{x=787,m=2655,a=1222,s=2000}"))
+
+        // assert
+        assertThat(key).isEqualTo("qqz")
+    }
+
+    @Test
+    fun should_create_part() {
+
+        // act
+        val part = Part("{x=787,m=2655,a=1222,s=2876}")
+
+        // assert
+        assertThat(part.x).isEqualTo(787)
+        assertThat(part.m).isEqualTo(2655)
+        assertThat(part.a).isEqualTo(1222)
+        assertThat(part.s).isEqualTo(2876)
+    }
+
+    @Test
+    fun should_create_workflow() {
+        // arrange
+
+        // act
+        val workflow = Workflow("qqz{s>2770:qs,m<1801:hdj,R}")
+
+        // assert
+        assertThat(workflow.key).isEqualTo("qqz")
+        assertThat(workflow.rules).contains(
+            Rule("s>2770:qs"),
+            Rule("m<1801:hdj"),
+            Rule("R"),
+        )
+    }
+
+    @Test
+    fun should_create_rule_with_field() {
+        // act
+        val rule = Rule("s>2770:qs")
+
+        // assert
+        assertThat(rule.field).isEqualTo("s")
+        assertThat(rule.operator).isEqualTo('>')
+        assertThat(rule.value).isEqualTo(2770)
+        assertThat(rule.next).isEqualTo("qs")
+    }
+
+    @Test
+    fun should_create_rule_with_next() {
+        // act
+        val rule = Rule("srfg")
+
+        // assert
+        assertThat(rule.field).isNull()
+        assertThat(rule.operator).isNull()
+        assertThat(rule.value).isNull()
+        assertThat(rule.next).isEqualTo("srfg")
+    }
+
 }
