4a3571d0097df335c8e69741c3ecd9328363ba99
Torsten Wiederkehr <torsten.wiederkehr@ing.com>
2024-03-14 08:01:36 +0100

    Load lines with workflows and parts

--- src/main/kotlin/Day19.kt
+++ src/main/kotlin/Day19.kt
@@ -1,2 +1,15 @@
+import java.nio.file.Path
+
 class Day19 {
+
+    fun loadWorkflows(path: Path): List<String> {
+        val lines = Resources.resourceAsListOfString(path.toFile().name)
+        return lines.subList(0, lines.indexOf(""))
+    }
+
+    fun loadParts(path: Path): List<String> {
+        val lines = Resources.resourceAsListOfString(path.toFile().name)
+        return lines.subList(lines.indexOf("") + 1, lines.size)
+    }
+
 }
--- src/test/kotlin/Day19Test.kt
+++ src/test/kotlin/Day19Test.kt
@@ -1,2 +1,26 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.nio.file.Paths
+
 class Day19Test {
+
+    @Test
+    fun should_load_workflows() {
+
+        // act
+        val workflows = Day19().loadWorkflows(Paths.get("src", "test", "resources", "Day19_TestData.txt"))
+
+        // assert
+        assertThat(workflows).hasSize(11)
+    }
+
+    @Test
+    fun should_load_parts() {
+        // act
+        val parts = Day19().loadParts(Paths.get("src", "test", "resources", "Day19_TestData.txt"))
+
+        // assert
+        assertThat(parts).hasSize(5)
+
+    }
 }
