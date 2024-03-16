c25148cbea8533b51370e0b2185f456853aab2ff
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-03-16 05:34:49 +0100

    Implemented find starting position

--- src/main/kotlin/Day21.kt
+++ src/main/kotlin/Day21.kt
@@ -1,2 +1,14 @@
+import java.nio.file.Path
+
 class Day21 {
+
+    fun loadMapOfGarden(path: Path): List<String> {
+        return Resources.resourceAsListOfString(path.toFile().name)
+    }
+
+    fun startingPosition(mapOfGarden: List<String>): Point2D {
+        val y = mapOfGarden.indexOfFirst { it.contains("S") }
+        val x = mapOfGarden[y].indexOfFirst { it == 'S' }
+        return Point2D(x, y)
+    }
 }
--- src/test/kotlin/Day21Test.kt
+++ src/test/kotlin/Day21Test.kt
@@ -1,2 +1,54 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.nio.file.Paths
+
 class Day21Test {
+
+    private val mapOfGarden = listOf(
+        "...........",
+        ".....###.#.",
+        ".###.##..#.",
+        "..#.#...#..",
+        "....#.#....",
+        ".##..S####.",
+        ".##..#...#.",
+        ".......##..",
+        ".##.#.####.",
+        ".##..##.##.",
+        "..........."
+    )
+
+    @Test
+    fun should_load_map_of_garden() {
+        // act
+        val mapOfGarden = Day21().loadMapOfGarden(Paths.get("src", "test", "resources", "Day21_TestData.txt"))
+
+        // assert
+        assertThat(mapOfGarden).hasSize(11)
+    }
+
+    @Test
+    fun should_find_starting_position_on_smallest_map() {
+        // arrange
+        val smallestMap = listOf(
+            "...",
+            "S..",
+            "..#",
+        )
+
+        // act
+        val startingPosition = Day21().startingPosition(smallestMap)
+
+        // assert
+        assertThat(startingPosition).isEqualTo(Point2D(0, 1))
+    }
+
+    @Test
+    fun should_find_starting_position_on_garden_map() {
+        // act
+        val startingPosition = Day21().startingPosition(mapOfGarden)
+
+        // assert
+        assertThat(startingPosition).isEqualTo(Point2D(5, 5))
+    }
 }
