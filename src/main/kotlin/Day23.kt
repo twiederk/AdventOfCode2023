4dfb06bb1e5fe5e3ebddd647cc31c252a4baf8db
Torsten Wiederkehr <torsten.wiederkehr@ing.com>
2024-03-19 14:50:24 +0100

    Implemented load of test data

--- src/main/kotlin/Day23.kt
+++ src/main/kotlin/Day23.kt
@@ -1,2 +1,7 @@
+import java.nio.file.Path
+
 class Day23 {
+    fun loadMapOfHikingTrails(path: Path): List<String> {
+        return Resources.resourceAsListOfString(path.toFile().name)
+    }
 }
--- src/test/kotlin/Day23Test.kt
+++ src/test/kotlin/Day23Test.kt
@@ -1,2 +1,17 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.nio.file.Paths
+
 class Day23Test {
+
+    @Test
+    fun should_load_map_of_hiking_trails() {
+
+        // act
+        val mapOfHikingTrails =
+            Day23().loadMapOfHikingTrails(Paths.get("src", "test", "resources", "Day23_TestData.txt"))
+
+        // assert
+        assertThat(mapOfHikingTrails).hasSize(23)
+    }
 }
